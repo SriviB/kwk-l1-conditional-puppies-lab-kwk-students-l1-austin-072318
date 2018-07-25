d7221729d889c57c544c3c77b9261a1372d41d0a
SriviB <srivibalaji33@gmail.com>
2018-07-25 14:44:16 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -16,7 +16,7 @@ def less_puppies(pup_num)
   elsif pup_num == 3
     "Slow down! No more puppies."
   else
-    "Get back on your animal restriction!"
+    "Get back to your animal goals!"
   end
 end
 
